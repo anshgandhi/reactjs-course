7b9fab7763dd0d30d2a15e8a8b8a8ac48504f04f
Ansh Gandhi <anshgandhi16@gmail.com>
2021-11-20 13:01:18 -0500

    sending states assignment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,41 @@
 import Card from "./../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import { useState } from "react";
 
 function Expenses(props) {
-
   const data = props.data_;
 
+  const [filterYear, setFilterYear] = useState("2019");
+
+  const filterChangeHandler = (data) => {
+    setFilterYear((prevState) => {
+      return data;
+    });
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={data[0].title}
-        amount={data[0].amount}
-        date={data[0].date}
-      />
-      <ExpenseItem
-        title={data[1].title}
-        amount={data[1].amount}
-        date={data[1].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filterYear}
+        />
+        <ExpenseItem
+          // title={data[0].title}
+          title={filterYear}
+          amount={data[0].amount}
+          date={data[0].date}
+        />
+        <ExpenseItem
+          //title={data[1].title}
+          title={filterYear}
+          amount={data[1].amount}
+          date={data[1].date}
+        />
+      </Card>
+    </div>
   );
 }
 
