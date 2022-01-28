a8436357dc412b379a3216d787bc48f8c61392cb
Ansh Gandhi <anshgandhi16@gmail.com>
2022-01-27 21:18:18 -0500

    conditional content solution

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,11 +13,27 @@ const NewExpense = (props) => {
     };
     console.log(expenseData);
     props.onAddExpenseHandler(expenseData);
+    setIsEditing(false);
   };
 
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+
+  // dynamic expressions
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler}  />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler}  onCancel={stopEditingHandler}/>
+      )}
     </div>
   );
 };
