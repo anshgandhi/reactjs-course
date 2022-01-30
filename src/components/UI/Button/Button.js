a718746b48adb5930c21654b07e0e8524d2dcbb8
Ansh Gandhi <anshgandhi16@gmail.com>
2022-01-30 17:49:29 -0500

    styled components - backticks

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// 'button' is a special kind method on 'styled' object which is called using backticks.
+// this '``' is also called 'Tagged Template Literals'. JavScript feature.
+// 'styled' component has methods for all HTML elements. e.g. for <div>
+// we can use styled.div``;
+
+// so styled.button would return a Button element with styles that we define inside the backticks.
+const Button = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+    &:focus {
+      outline: none;
+    }
+
+    &:hover,
+    &:active {
+      background: #ac0e77;
+      border-color: #ac0e77;
+      box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    }
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
