73c3783ab17fca5d66cfaa122b167969065d6640
Ansh Gandhi <anshgandhi16@gmail.com>
2022-02-19 16:59:19 -0500

    pre useEffect

--- login_page/src/App.js
+++ login_page/src/App.js
@@ -7,9 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const userLoggedIn = localStorage.getItem('isLoggedIn');
+
+  // Avoiding this approach because this leads to infinite loop
+  // as 'setIsLoggedIn' triggers re-eval of the component.
+  // As 'userLoggedIn' is 'true' and because of that line 18 is 'true' and
+  // calls 'setIsLoggedIn' again, and so on infinite times.
+
+  // localStorage.getItem() always returns a 'string', even if the input was a boolean.
+  if (userLoggedIn === 'true'){
+    setIsLoggedIn(true);
+  }
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', true);
     setIsLoggedIn(true);
   };
 
