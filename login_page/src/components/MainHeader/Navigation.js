3fd401627feab2275d071ff0f91b9a994857e86b
Ansh Gandhi <anshgandhi16@gmail.com>
2022-02-19 21:57:17 -0500

    contextData logout

--- login_page/src/components/MainHeader/Navigation.js
+++ login_page/src/components/MainHeader/Navigation.js
@@ -21,7 +21,7 @@ const Navigation = (props) => {
         )}
         {contextData.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={contextData.onLogout}>Logout</button>
           </li>
         )}
       </ul>
